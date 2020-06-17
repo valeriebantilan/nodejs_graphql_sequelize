4298a0c689cba7f11bf1598c8ffec0843ccf935c
valerie_bantilan <valerie_bantilan@ligph.com>
2020-06-17 18:08:10 +0800

    Integrate unit test for create customer

--- test/Customer.test.js
+++ test/Customer.test.js
@@ -7,15 +7,51 @@ const request = supertest(app);
 
 describe('GraphQL', () => {
     // Tests
+    let customer = {
+            firstName: "Venxel",
+            lastName: "Bantilan",
+            email: "venxel@gmail.com"
+    };
 
-    it('Returns all customers', (done) => {
+    it('Return specific customer by ID', (done) => {
         request.post('/graphql')
-        .send({ query: '{ customers { id firstName lastName } }' })
-        .set("Accept", "application/json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .end((err, res) => {
+            .send({ query: `{ customer(id: ${1}) { id firstName lastName } }` })
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end((err, res) => {
+                // res will contain array of all users
+                if (err) return done(err);
+                // assume there are a 100 users in the database
+                console.log(res.body);
+                expect(res.body).to.be.an.instanceof(Object);
+                done();
+            })
+    });
+
+    it('Create a customer', (done) => {
+        request.post('/graphql')
+            .set('content-type', 'application/json')
+            .send({
+                query: `mutation createCustomer($input: CreateCustomerInput!)
+                {
+                createCustomer(input: $input) 
+                    {
+                        firstName,
+                        lastName,
+                        email
+                    }
+                }
+                `,
+                "variables": {
+                    'input': customer,
+                }
+            })
+            .expect(200)
+            .end((err, res) => {
+            
             // res will contain array of all users
+                console.log(err);
             if (err) return done(err);
             // assume there are a 100 users in the database
             console.log(res.body);
